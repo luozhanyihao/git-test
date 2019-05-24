d8676f6dd5ce06afc0eae459811a82e9320cc36c
luozhan <592806680@qq.com>
2019-05-24 21:03:12 +0800

    本地工程

--- .idea/src/main/java/FirstClass.java
+++ .idea/src/main/java/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
         System.out.println("helloproject");
         System.out.println("helloproject");
         System.out.println("hello");
+        System.out.println("12");
     }
 
 }
